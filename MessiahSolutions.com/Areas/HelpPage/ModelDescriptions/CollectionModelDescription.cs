5895433dc68d665e496b2a52a270bb07cee015f7
vadimrasin <hsvd.rv@gmail.com>
2019-11-06 16:09:08 +0200

    removed the previous aspnetCore2.1 messiahsoftwaresolutinos.com and opened a .net framework / MVC app for .net 4.7.2 to run tests / check publish profile (local publish profile working!)

---
+++ MessiahSolutions.com/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MessiahSolutions.com.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MessiahSolutions.com/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MessiahSolutions.com.MvcApplication" Language="C#" %>
